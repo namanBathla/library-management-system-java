357d23610c5e90e7f34d45885e6fe59f9094c14f
Naman <namanuser1307@gmail.com>
2026-02-02 20:08:51 +0530

    fix conflict

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
         String url = "jdbc:mysql://localhost:3306/phonebook";
         String user = "root";
         String password = "Mysql@1234";
 
         try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection con = DriverManager.getConnection(url, user, password);
             Statement stmt = con.createStatement();
@@ -18,8 +18,10 @@ public class Main {
                 System.out.println(rs.getString("name"));
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
